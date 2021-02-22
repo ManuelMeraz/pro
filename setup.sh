70b00f092a548ecdb060278fbe746697358fb820
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-22 09:45:43 -0500

    enable sourcing of setup.sh when in container

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-if ! groups | grep docker &> /dev/null; then
+if ! groups | grep docker &> /dev/null && ! hostname | grep docker &> /dev/null; then
     echo "You must have permission to execute docker. Add yourself to the docker group with the following command:"
     echo 
     echo "sudo usermod -aG docker $USER"
     echo 
     echo "Then log out and back in or restart your machine."
-    return 1
 fi
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
